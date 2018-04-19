6001a3cf033f5ce15cfdb753501ad100c4c9e598
co-deion <cdphicks.0@gmail.com>
2018-04-19 01:21:05 -0500

    initial commit

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Rank = () => {
+    return (
+        <div className=''>
+            <div className='white f3'>
+                {"YO! You're currently ranked..."}
+            </div>
+            <div className='white f3'>
+                {"#3"}
+            </div>
+        </div>
+    )
+}
+
+export default Rank
